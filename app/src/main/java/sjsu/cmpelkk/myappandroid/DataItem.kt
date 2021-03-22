09829ba811858d13f659a25a433f4ecd9fe2be67
Sania Gonsalves <sanialinus.gonsalves@sjsu.edu>
2021-03-21 17:06:51 -0700

    commit changes

--- app/src/main/java/sjsu/cmpelkk/myappandroid/DataItem.kt
+++ app/src/main/java/sjsu/cmpelkk/myappandroid/DataItem.kt
@@ -12,19 +12,9 @@ data class DataItem(
     val imagename: String //Int, //String?
 ): Serializable
 
-//val yourDrawable  = ResourcesCompat.getDrawable(resources, R.drawable.sjsu1, null)
-//Uri.parse("android.resource://" + packageName + "/" + R.drawable.sjsu1)
-
-//public val carddefaultdata = listOf<DataItem>(
-//    DataItem("name1", "testtitle1", "Test story, Test storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest story", false, 4, R.drawable.imageupload),
-//    DataItem("name2", "testtitle2", "Test story", true, 4, R.drawable.sjsu1),
-//    DataItem("name3", "testtitle3", "Test story", false, 4, R.drawable.sjsu2),
-//    DataItem("name4", "testtitle4", "Test story", false, 4, R.drawable.sjsu3)
-//    )
-
 public val carddefaultdata = listOf<DataItem>(
-        DataItem("name1", "testtitle1", "Test story, Test storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu1).toString()),
-        DataItem("name2", "testtitle2", "Test story", true, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu1).toString()),//Uri is not serializable
-        DataItem("name3", "testtitle3", "Test story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu2).toString()),
-        DataItem("name4", "testtitle4", "Test story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu3).toString())
+        DataItem("Burger", "Burger", "Make a burger using bread and meat patty \n \n \n Ingredients: \n \n Bread \n \n Cheese \n \n Veggies \n \n Meat Patty \n \n Sauce \n\n  ", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.burger).toString()),
+        DataItem("Sandwich", "Sandwich", "Make a sandwich using sliced bread and veggies", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sandwich).toString()),
+        DataItem("Pancakes", "Pancakes", "Make pancake using pancake batter and maple syrup", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.pancakes).toString()),
+        DataItem("Dessert", "Dessert", "Make dessert using chocolate and decorate with berries", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.dessert).toString())
 )
